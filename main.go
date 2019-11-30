8db9f7a9ece62a78ee8173710c32c7fc747671d1
Nikita Svirin <nik200494@rambler.ru>
2019-11-30 19:00:18 +0300

    Добавил чтение файла построчно без обработки ошибок

--- main.go
+++ main.go
@@ -1,13 +1,34 @@
 package main
 
+import (
+	"bufio"
+	"os"
+)
+
 func readFile(in <-chan string) <-chan string {
 	out := make(chan string)
 	go func() {
+		defer close(out)
 		fileName, ok := <-in
 		if ok {
-			// scan file
+			file, err := os.Open(fileName)
+			if err != nil {
+				// process open file error
+				return
+			}
+			defer file.Close()
+
+			scanner := bufio.NewScanner(file)
+			for scanner.Scan() {
+				out <- scanner.Text()
+			}
+
+			if err := scanner.Err(); err != nil {
+				// process scanner error
+			}
+		} else {
+			// process in channel closing
 		}
-		close(out)
 	}()
 	return out
 }
