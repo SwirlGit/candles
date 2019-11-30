531d95c54eeb7bb8f6c1fcb7cb4bdcce412c2cd5
Nikita Svirin <nik200494@rambler.ru>
2019-11-30 18:42:19 +0300

    Инициализация пайплайна

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+func readFile(in <-chan string) <-chan string {
+	out := make(chan string)
+	return out
+}
 
+func parseLine(in <-chan string) <-chan string {
+	out := make(chan string)
+	return out
+}
+
+func saveLine(in <-chan string) <-chan string {
+	out := make(chan string)
+	return out
+}
+
+func main() {
+	// setup pipeline
+	in := make(chan string)
+	fileLinesOut := readFile(in)
+	parsedLinesOut := parseLine(fileLinesOut)
+	finalOut := saveLine(parsedLinesOut)
+
+	<-finalOut
 }
