d8329e1d2a13bdbff873cc7b76557255cbbf51ff
Nikita Svirin <nik200494@rambler.ru>
2019-11-30 18:55:32 +0300

    Добавил go функции

--- main.go
+++ main.go
@@ -2,16 +2,36 @@ package main
 
 func readFile(in <-chan string) <-chan string {
 	out := make(chan string)
+	go func() {
+		fileName, ok := <-in
+		if ok {
+			// scan file
+		}
+		close(out)
+	}()
 	return out
 }
 
 func parseLine(in <-chan string) <-chan string {
 	out := make(chan string)
+	go func() {
+		for line := range in {
+			// parseSomeHow
+			// out <- parsedLine
+		}
+		close(out)
+	}()
 	return out
 }
 
 func saveLine(in <-chan string) <-chan string {
 	out := make(chan string)
+	go func() {
+		for line := range in {
+			// save line to file
+		}
+		close(out)
+	}()
 	return out
 }
 
